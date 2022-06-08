05f2fc85582784ba5c02d84cb9489e08be77b350
Ephraim Osse <101394861+Ephraimosse9@users.noreply.github.com>
2022-06-08 15:20:46 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
