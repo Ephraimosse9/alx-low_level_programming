59582aeefd4be4edeb4d750011ac605241f792bc
Ephraim Osse <101394861+Ephraimosse9@users.noreply.github.com>
2022-06-08 15:40:27 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
-#include "main.c"
-
+#include "main.h"
+Int main(void)
+{
 /**
   * print_alphabet - Make the alphabet
   *
   * Return: void
   */
 void print_alphabet(void)
-{
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
