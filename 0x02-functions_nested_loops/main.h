2132ce66e112822f250c03f59d4a4020cbbe6667
Ephraim Osse <101394861+Ephraimosse9@users.noreply.github.com>
2022-06-08 16:33:57 +0100

    Update main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+int _putchar (char);
+void print alphabet (void);
+void print alphabet x10(void);
+int
+islower (int c) ;
+int
+isalpha(int c);
+int print_sign(int n);
+int abs(int);
+int print last digit(int nld);
+void jack bauer(void);
+void times table(void);
+int add (int one, int two);
+void print to 98 (int n);
+void print times table(int n);
