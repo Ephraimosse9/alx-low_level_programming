fe09870a309842df361e8518e0ad691d91d19bc9
Ephraim Osse <101394861+Ephraimosse9@users.noreply.github.com>
2022-06-16 01:57:40 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	for(j = 0; dest[j] != '\0'; j++);
-	while(str[i] != '\0' && i < n)
+	for (j = 0; dest[j] != '\0'; j++)
+		;
+	while (src[i] != '\0' && i < n)
 	{
-		dest[j + i] = str[i];
+		dest[j + i] = src[i];
 		i++;
 	}
-	return (dest)
+	return (dest);
 }
